a36f749112636dcc249f1db6547a409b9553e835
San <anandjaisy@gmail.com>
2020-04-29 17:04:26 +1000

    "added public api"

--- projects/falcon-core/src/public-api.ts
+++ projects/falcon-core/src/public-api.ts
@@ -2,10 +2,37 @@
  * Public API Surface of falcon-core
  */
 
- //Module
+ /*
+ * Public API Surface of falcon-core-module
+ */
 export * from './lib/falcon-core.module';
 export * from './lib/module/angular-material/angular-material.module';
 
-
-//Service
+ /*
+ * Public API Surface of falcon core module service
+ */
 export * from './lib/service/igeneric-http-client';
+
+ /*
+ * Public API Surface of falcon core module component
+ */
+export * from './lib/component/reactive-controls/reactive-controls.component';
+export * from './lib/component/textbox/textbox.component';
+export * from './lib/component/text-area/text-area.component';
+export * from './lib/component/radio/radio.component';
+export * from './lib/component/select/select.component';
+export * from './lib/component/slide-toggle/slide-toggle.component';
+export * from './lib/component/slider/slider.component';
+export * from './lib/component/date-picker/date-picker.component';
+export * from './lib/component/delete-dialog/delete-dialog.component';
+export * from './lib/component/checkbox/checkbox.component';
+export * from './lib/component/progress-bar/progress-bar.component';
+export * from './lib/component/progress-spinner/progress-spinner.component';
+export * from './lib/component/bottom-sheet/bottom-sheet.component';
+export * from './lib/component/button-toggle/button-toggle.component';
+export * from './lib/component/button/button.component';
+
+ /*
+ * Public API Surface of falcon core module directive
+ */
+export * from './lib/directive/reactive-field.directive';
