594e72b09a27285daea53adf5728126b9e47feb9
Anand Jaisy <ananda.jaisy@playtimesolutions.com.au>
2020-12-23 20:01:44 +0530

    "Interceptor error handling"

--- projects/falcon-core/src/lib/service/http/httpInterceptor.ts
+++ projects/falcon-core/src/lib/service/http/httpInterceptor.ts
@@ -1,13 +1,9 @@
 import { Injectable } from '@angular/core';
-import {
-    HttpRequest,
-    HttpHandler,
-    HttpEvent,
-    HttpInterceptor
-} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 import { AuthService } from '../open-id/auth.service';
 import { LoggerService } from '../logger.service';
+import { catchError } from "rxjs/internal/operators";
 
 @Injectable({
     providedIn: 'root'
@@ -20,11 +16,21 @@ export class HttpFalconInterceptor implements HttpInterceptor {
         const authToken = this.authService.getAuthorizationHeaderValue();
         this.logger.info("Auth bearer token ", authToken);
 
-        const authRequest = request.clone({
-            setHeaders: {
-                Authorization: authToken
-            }
-        });
-        return next.handle(authRequest);
+        if (authToken) {
+            request = request.clone({
+                setHeaders: {
+                    Authorization: authToken
+                }
+            });
+        }
+
+        /**
+            * continues request execution
+        */
+        return next.handle(request).pipe(catchError((error, caught) => {
+            //intercept the respons error and displace it to the console
+            this.logger.error(error);
+            return of(error);
+        }) as any);
     }
 }
