b3c001c38cfe1cee729f0d48737cd34783d971a2
anandjaisy <anandjaisy@gmail.com>
2022-07-06 08:28:45 +1000

    Version update to 0.0.5

--- projects/falcon-core/src/lib/component/editor/editor.component.ts
+++ projects/falcon-core/src/lib/component/editor/editor.component.ts
@@ -1,20 +1,18 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IComponentConfig } from '../../model/imeta';
 import { UntypedFormGroup } from '@angular/forms';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'falcon-text-editor',
   templateUrl: './editor.component.html',
-  styleUrls: ['./editor.component.css']
+  styleUrls: ['./editor.component.css'],
 })
 export class EditorComponent implements OnInit {
   @Input() field: IComponentConfig;
   @Input() group: UntypedFormGroup;
-  editor = ClassicEditor;
-  constructor() { }
+  public editor = ClassicEditor;
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
